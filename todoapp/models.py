e599615b89d76a5d39e6b0d9fb2fbb8f1c26388a
Nikita <lanfor96@yandex.by>
2021-04-21 16:40:38 +0300

    todoapp

--- todoapp/models.py
+++ todoapp/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    complete = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-    PRIORITY_CHOISES = (('1', 'Высокий'), ('2', 'Средний'), ('3', 'Низкий'))
-    priority = models.CharField(max_length=10, choices=PRIORITY_CHOISES, null=True)
+    title = models.CharField('Название', max_length=200)
+    description = models.TextField('Описание', null=True, blank=True)
+    complete = models.BooleanField('Выполнение', default=False)
+    created = models.DateTimeField('Создана', auto_now_add=True)
+    PRIORITY_CHOISES = (('Высокий', 'Высокий'), ('Средний', 'Средний'), ('Низкий', 'Низкий'))
+    priority = models.CharField('Приоритет', max_length=10, choices=PRIORITY_CHOISES, null=True)
 
     def __str__(self):
         return self.title
